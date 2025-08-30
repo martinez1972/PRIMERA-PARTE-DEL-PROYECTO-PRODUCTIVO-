146fd9a1f2454e465ecefde88ee1373377615772
martinez <juanjosemartinezvallejo2@gmail.com>
2025-08-30 12:12:03 -0500

    primera parte

---
+++ database/migrations/2025_08_30_151531_create_personas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('personas', function (Blueprint $table) {
+            $table->id();
+            $table->string('red_social',80);
+            $table->string('direccion',80);
+            $table->string('tipo_persona',20);
+            $table->tinyInteger('estado')->default(1);
+            $table->foreignId('documento_id')->unique()->costrained('documento')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('personas');
+    }
+};
---
+++ database/migrations/2025_08_30_163204_create_categoria_producto_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categoria_producto', function (Blueprint $table) {
+            $table->id();
+             $table->foreignId('productos_id')->costrained('productos')->onDelete('cascade' );
+              $table->foreignId('categoria_id')->costrained('categoria')->onDelete('cascade' );
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categoria_producto');
+    }
+};
